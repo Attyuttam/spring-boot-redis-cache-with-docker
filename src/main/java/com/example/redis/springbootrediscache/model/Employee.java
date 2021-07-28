9e97ad44b04f1fe559cb88dfe559ee967ba4c031
attyuttam <attyuttam@gmail.com>
2021-07-28 11:34:24 +0530

    Enabled Redis distrbuted caching using bitnami redis image using docker

--- src/main/java/com/example/redis/springbootrediscache/model/Employee.java
+++ src/main/java/com/example/redis/springbootrediscache/model/Employee.java
@@ -1,23 +1,41 @@
 package com.example.redis.springbootrediscache.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
+@Getter
+@Setter
+@Table(name = "employee")
 public class Employee implements Serializable {
-
     @Id
-    @GeneratedValue
-    private int id;
+    @Column(name = "employee_id")
+    private Integer id;
+
+    @Column(name="department_id")
+    private Integer department;
+
+    @Column(name = "employee_name")
     private String name;
 
+    @Column(name = "employee_role")
+    private String role;
 }
+
